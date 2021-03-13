85e48d87cf9e13fb59c1dbebc62e76d2bb2644a3
Sally-A <Sally.Anderson@gmx.com>
2021-03-13 11:18:12 -0500

    week2 task2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -13,12 +13,12 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <Card title='Contact Information' wrapperStyle={{margin: 20}}>
+                <Card title='Contact Inforamtion' wrapperStyle={{margin:20}} >
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
-                    <Text>U.S.A.</Text>
+                    <Text style={{marginBottom:10}}>U.S.A</Text>
                     <Text>Phone: 1-206-555-1234</Text>
-                    <Text>Email: campsites@nucamp.co</Text>
+                    <Text>Email: campsite@numcamp.co</Text>
                 </Card>
             </ScrollView>
         );
@@ -27,3 +27,5 @@ class Contact extends Component {
 
 
     export default Contact;
+
+
